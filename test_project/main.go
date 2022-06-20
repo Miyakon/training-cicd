f02031e8cb2a1b22ebe4532b6501aea24930c6d6
Miyakon <Miyako0901t@gmail.com>
2022-06-21 08:07:16 +0900

    Training: treat strings

--- test_project/main.go
+++ test_project/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+	"log"
+)
 
 func main() {
 	fmt.Printf("Hello world\n")
@@ -19,11 +24,48 @@ func main() {
 		"in Go. \n"
 
 	fmt.Printf("%s", multi_line)
+
+	// fmt.Printf(GetHogeByGlue("piyo"))
+	mystr := GetHogeByGlue("piyo")
+	fmt.Printf("%s", mystr)
+
+	fmt.Printf("%s", GetHogeBySprintf("piyopiyo"))
+
+	fmt.Printf("%s", GetHogeByTemplate("piyopiyopiyo"))
+
 }
 
 // 関数定義と文字列+変数
 func GetHogeByGlue(piyo string) string {
 	return "fuga " + piyo + " mogera" + "\n"
+}
+
+// フォーマット指定
+func GetHogeBySprintf(piyo string) string {
+	return fmt.Sprintf("fuga %s mogera\n", piyo)
+}
+
+// Template関数
+
+type FieldsToReplace struct {
+	Replace1 string
+}
+
+func GetHogeByTemplate(piyo string) string {
+	var msg_result bytes.Buffer
+
+	msg_tpl, msg_err := template.New("myTemplate").Parse("fuga {{.Replace1}} mogera\n")
+	if msg_err != nil {
+		log.Fatal(msg_err)
+	}
+	replace_to := FieldsToReplace{
+		Replace1: piyo,
+	}
+	if msg_err := msg_tpl.Execute(&msg_result, replace_to); msg_err != nil {
+		log.Fatal(msg_err)
+	}
+
+	return msg_result.String()
 }
 
 // コメント
