c9f3c4a96a7630993381d8a14eaf2de73da2d65a
Miyakon <Miyako0901t@gmail.com>
2022-06-22 07:58:49 +0900

    Apply: goimports

--- gin_test/main.go
+++ gin_test/main.go
@@ -1,15 +1,17 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
 
-import "net/http"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-    engine:= gin.Default()
-    engine.GET("/", func(c *gin.Context) {
-        c.JSON(http.StatusOK, gin.H{
-            "message": "hello world",
-        })
-    })
-    engine.Run(":3000")
+	engine := gin.Default()
+	engine.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "hello world",
+		})
+	})
+	engine.Run(":3000")
 }
